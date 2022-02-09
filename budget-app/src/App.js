e1f37e99e52fd2ff5e00d2964df40e059a6836d5
Julio Santacruz <aram.jsh1@gmail.com>
2022-02-08 21:12:58 -0800

    primer commit

---
+++ budget-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="frosted-box">
+      <h1>Budget App Js</h1>
+      <h3>On contruction</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
